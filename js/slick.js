29716d403f5beaa2c2a7a415afed61ea8f0a2840
Kaisei <e225721@ie.u-ryukyu.ac.jp>
2025-02-04 01:21:02 +0900

    comp page

--- js/slick.js
+++ js/slick.js
@@ -1,14 +1,12 @@
+// スライドショー
+$(function(){
+  $('.mainimg').slick({
+    infinite: true,       // スライドをループさせる
+    dots: true,           // 下部にドットナビゲーションを表示
+    arrows: false,         // 前後の矢印を表示
+    autoplay: true,       // 自動再生
+    autoplaySpeed: 3000,  // 自動再生の速度
+    // 必要に応じてその他オプションを追加
+  });
+});
 
-
-// トップページスライドショー
-	$('.mainimg').slick({
-		autoplay: true,
-		dots: true,				//丸いページナビボタンを表示
-		arrows: false,			//左右の矢印
-		autoplaySpeed: 4000,	//切り替えのスピード。デフォルトは3000。
-		pauseOnHover: false,	//マウスオン時にスライドショーをストップする
-        
-        centerMode: true,
-        centerPadding: '50px',
-        focusOnSelect: true,
-    });
